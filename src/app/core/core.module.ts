60e6820744c9bbe4047440d8d3275e964da760b3
Bruno.duarte.silva <Bruno.duarte.silva@accenture.com>
2021-03-28 13:37:17 -0300

    adding navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,20 +5,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataBase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    //Shared components
+    NavbarComponent
   ]
 })
 export class CoreModule { }
